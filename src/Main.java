340f9ea9db42180b2682c3c7c95c48f5d10af67b
K1ndbeetle <alx-s@bk.ru>
2022-11-20 02:20:26 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -35,5 +35,41 @@ public class Main {
         if (people > 102) {
             System.out.println("Вагон уже полностью забит");
         }
+
+        //homework2
+        //task №4
+        System.out.println("Задача №4");
+        int years = 9;
+        if (years >= 18) {
+            System.out.println("Поздравляем с совершеннолетием");
+        }
+        else {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+
+        //task №5
+        System.out.println("Задача №5");
+        if (years >= 7 && years < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        else if (years >= 18 && years < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
+        else if (years > 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+
+        //task №6
+        System.out.println("Задача №6");
+        int passengers = 45;
+        if (passengers <= 60) {
+            System.out.println("У вас сидячее место");
+        }
+        else if (passengers > 60 && passengers <= 102) {
+            System.out.println("У вас стоячее место");
+        }
+        else {
+            System.out.println("Вагон уже полностью забит");
+        }
     }
 }
